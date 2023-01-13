dab58e0de07abc046616fb2a161ab82288a07a45
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2023-01-13 15:23:35 +0300

    creates basic files and structure

---
+++ src/errorHandlers.js
@@ -0,0 +1,34 @@
+export const badRequestHandler = (err, req, res, next) => {
+  // 400
+  if (err.status === 400) {
+    res.status(400).send({ message: err.message });
+  } else {
+    next(err);
+  }
+};
+
+export const unauthorizedHandler = (err, req, res, next) => {
+  // 401
+  if (err.status === 401) {
+    res.status(401).send({ message: err.message });
+  } else {
+    next(err);
+  }
+};
+
+export const notFoundHandler = (err, req, res, next) => {
+  // 404
+  if (err.status === 404) {
+    res.status(404).send({ message: err.message });
+  } else {
+    next(err);
+  }
+};
+
+export const genericErrorHandler = (err, req, res, next) => {
+  // 500
+  console.log("ERROR RECEIVED FROM UP ABOVE:", err);
+  res
+    .status(500)
+    .send({ message: "An error occurred on our side! we gonna fix that asap" });
+};
---
+++ src/server.js
@@ -0,0 +1,52 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import mediasRouter from "./api/medias/index.js";
+import { join } from "path";
+import cors from "cors";
+import {
+  badRequestHandler,
+  genericErrorHandler,
+  notFoundHandler,
+} from "./errorHandlers.js";
+
+const server = express();
+const port = process.env.PORT;
+
+const publicFolderPath = join(process.cwd(), "./public");
+
+//*******MIDDLEWARES***********/
+
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
+
+const corsOpts = {
+  origin: (origin, corsNext) => {
+    console.log("CURRENT ORIGIN: ", origin);
+    if (!origin || whitelist.indexOf(origin !== -1)) {
+      //if current origin is in the whitelist you can move on
+      corsNext(null, true);
+    } else {
+      corsNext(
+        createHttpError(400, `Origin ${origin} is not in the whitelist!`)
+      );
+    }
+  },
+};
+
+server.use(cors(corsOpts));
+server.use(express.json());
+server.use(express.static(publicFolderPath));
+
+//*******ENDPOINTS*************/
+
+server.use("/medias", mediasRouter);
+
+//*******ERROR HANDLERS********/
+server.use(badRequestHandler); //400
+server.use(notFoundHandler); //401
+server.use(notFoundHandler); //404
+server.use(genericErrorHandler); //500
+
+server.listen(port, () => {
+  console.table(listEndpoints(server));
+  console.log("Server is running on port:", port);
+});
